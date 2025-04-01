fb2be14aa9479f88ca9aa2ed0a5d947cb6e7a844
design-with-stephen <stephen211.me@gmail.com>
2025-04-01 11:59:26 +0300

    created the about and blog pages

---
+++ front-end/scripts/blog.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const blogPosts = document.querySelectorAll(".blogPost");
+    const seeMoreBtn = document.querySelector(".discoverMoreBtn");
+    
+    let postsToShow = 3;
+    let expanded = false;
+    
+    function updatePostsDisplay() {
+        blogPosts.forEach((post, index) => {
+            if (index < postsToShow) {
+                post.style.display = "block";
+            } else {
+                post.style.display = "none";
+            }
+        });
+        seeMoreBtn.innerText = expanded ? "Show Less" : "Discover More";
+    }
+    
+    updatePostsDisplay();
+    
+    seeMoreBtn.addEventListener("click", function () {
+        if (expanded) {
+            postsToShow = 3;
+        } else {
+            postsToShow = blogPosts.length;
+        }
+        expanded = !expanded;
+        updatePostsDisplay();
+    });
+});
