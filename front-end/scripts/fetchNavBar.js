e3c88e17105d8375fdbfa43e1b462244970cd745
design-with-stephen <stephen211.me@gmail.com>
2025-04-06 11:13:13 +0300

    fixed navbar bugs

--- front-end/scripts/fetchNavBar.js
+++ front-end/scripts/fetchNavBar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("./navbar.html")
+    fetch("../pages/navbar.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar-container").innerHTML = data;
